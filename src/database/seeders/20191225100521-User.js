0ec20c33340e765e0735d30fb2b53a8debf53827
sharkdevs <mesharkz1@gmail.com>
2019-12-27 09:27:21 +0300

    feat-add-db(Sequelize): add sequelize orm
    - create a config file
    - install sequelize
    - create a sample model migration and seeders
    - test connection
    
    [Finishes ft]

---
+++ src/database/seeders/20191225100521-User.js
@@ -0,0 +1,28 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    /*
+      Add altering commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+     */
+      return queryInterface.bulkInsert('Users', [{
+        name: 'John Doe',
+        email: 'jd@gmail.com',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      }], {});
+  },
+
+  down: (queryInterface, Sequelize) => {
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.bulkDelete('User', null, {});
+    */
+  }
+};
